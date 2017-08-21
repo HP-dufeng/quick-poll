762d2245f5ef02def6483043e9e65383dd9a33a7
fengdu <fengdu_xiangyang@163.com>
2017-08-21 17:27:52 +0800

    init project

---
+++ src/main/java/com/example/repository/OptionRepository.java
@@ -0,0 +1,8 @@
+package com.example.repository;
+
+import com.example.domain.Option;
+import org.springframework.data.repository.CrudRepository;
+
+public interface OptionRepository extends CrudRepository<Option, Long> {
+
+}
